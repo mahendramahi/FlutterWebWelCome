c046dafd595918509a96a18e7f6fe024f79b984c
mahendras <mahendrasharma.sharma369@gmail.com>
2019-08-21 19:09:17 +0530

    web 2 commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,128 @@
+import 'dart:convert';
+import 'package:dio/dio.dart';
 import 'package:flutter_web/material.dart';
-
+import 'package:hummingbird/utils/Strings.dart';
+import 'package:http/http.dart' as http;
 void main() => runApp(Hummingbird());
 
 class Hummingbird extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Hummingbird - Flutter For Web',
+      title: Strings.appTitle,
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.teal,
       ),
       home: MyWebsite(),
+      //GHFlutter(),
+      //MyWebsite(),
     );
   }
 }
+
+// class GHFlutter extends StatefulWidget {
+//   @override
+//   _GHFlutterState createState() => _GHFlutterState();
+// }
+
+// class _GHFlutterState extends State<GHFlutter> {
+//  String nextPage = "https://swapi.co/api/people";
+//   var _scrollController = ScrollController();
+//   bool isLoading = false;
+//   List names = List();
+ 
+//   final dio = Dio();
+
+//   void _getMoreData() async {
+//     if (!isLoading) {
+//       setState(() {
+//         isLoading = true;
+//       });
+ 
+//       final response = await dio.get(nextPage);
+//       List tempList = List();
+//       nextPage = response.data['next'];
+//       for (int i = 0; i < response.data['results'].length; i++) {
+//         tempList.add(response.data['results'][i]);
+//       }
+
+//       setState(() {
+//         isLoading = false;
+//         names.addAll(tempList);
+//       });
+//     }
+//   }
+
+//   @override
+//   void initState() {
+//     this._getMoreData();
+//     // TODO: implement initState
+//     super.initState();
+//     _scrollController.addListener(() {
+//       if(_scrollController.position.pixels == _scrollController.position.maxScrollExtent) {
+//         _getMoreData();
+//       }
+//     });
+//   }
+
+// @override
+//   void dispose() {
+//     _scrollController.dispose();
+//     // TODO: implement dispose
+//     super.dispose();
+//   }
+
+// Widget _buildProgressBar() {
+//   return Padding(
+//     padding: const EdgeInsets.all(8.0),
+//     child: Center(
+//       child: Opacity(
+//         opacity: isLoading ? 1.0 : 0.0,
+//         child: CircularProgressIndicator(),
+//       ),
+//     ),
+//   );
+// }
+
+// Widget _buildList() {
+//   return ListView.builder(
+//     itemCount: names.length + 1,
+//     itemBuilder: (BuildContext context, ),
+//   );
+// }
+
+// }
 
 class MyWebsite extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              child: Text("Drawer Header"),
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+            ),
+            ListTile(
+              title: Text("Tiles"),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: Text("Tiles 2"),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            )
+          ],
+        ),
+      ),
       appBar: AppBar(
         title: Text('Hummingbird'),
         centerTitle: true,
---
+++ lib/utils/Strings.dart
@@ -0,0 +1,4 @@
+class Strings {
+  static String appTitle = "My First Web";
+  static String nextPage = "https://swapi.co/api/people";
+}
